f94a01942c603288794b884ead9d88c5f192a316
Höhn, Christoph <christoph.hoehn@accenture.com>
2025-01-30 22:14:36 +0100

    hint for chat ex

--- src/main/java/com/educoach/chat/ChatResourceV1.java
+++ src/main/java/com/educoach/chat/ChatResourceV1.java
@@ -28,6 +28,7 @@ public class ChatResourceV1 {
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/completion")
     public String completion(String prompt) {
 
         return chatGpt.completion(getBearer(),
@@ -37,6 +38,29 @@ public class ChatResourceV1 {
         .choices().toString();
 
     }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/create-lesson")
+    public String createLesson(String topic) {
+
+        //tip, wie ein prompt aussehen könnte:
+        //`Imagine yourself as a teacher in a German Middle school from grade ${grade}. Your thoughts right now are that you want to create an exercise over: ${topic} in the Subject ${subject} based on the Online Reference: ${reference}. The tasksheet should be A4 size big with text size 12 and should include different tasks to train the subject as a whole. Make more tasks. Just show the sources, A4 tasks and solution sheet. Make more tasks. Return the answer as a template string of the html styled version. for example <h1>Hello</h1> would be a heading in the response string. Answer in German and leave out unnessecary formal contents.`;
+        
+        var promt = "Erstelle eine Lektion für die Themengruppe " + topic;
+
+        var response = chatGpt.completion(getBearer(),
+            ChatGptRequest.newRequest(openaiModel, promt))
+            .choices();
+        
+        return response.get(0).message().content();
+
+    }
+
+
+
+
 
     // Builds the Bearer token
 
