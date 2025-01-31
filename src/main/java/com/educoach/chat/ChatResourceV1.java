2813b958d0357f983e0561abc8dfc184498b1cb5
Höhn, Christoph <christoph.hoehn@accenture.com>
2025-01-31 12:48:26 +0100

    aufg_2

--- src/main/java/com/educoach/chat/ChatResourceV1.java
+++ src/main/java/com/educoach/chat/ChatResourceV1.java
@@ -47,8 +47,40 @@ public class ChatResourceV1 {
 
         //tip, wie ein prompt aussehen könnte:
         //`Imagine yourself as a teacher in a German Middle school from grade ${grade}. Your thoughts right now are that you want to create an exercise over: ${topic} in the Subject ${subject} based on the Online Reference: ${reference}. The tasksheet should be A4 size big with text size 12 and should include different tasks to train the subject as a whole. Make more tasks. Just show the sources, A4 tasks and solution sheet. Make more tasks. Return the answer as a template string of the html styled version. for example <h1>Hello</h1> would be a heading in the response string. Answer in German and leave out unnessecary formal contents.`;
+        var promt = String.format("""
+            Imagine yourself as a teacher in a German Middle school from grade 12. 
+        Your thoughts right now are that you want to create an lesson over: %s 
+        Plan a lesson for this topic.
+        Return the answer as a template string of the html styled version. 
+        for example <h1>Hello</h1> would be a heading in the response string. 
+        Answer in German and leave out unnessecary formal contents.""", topic);
+                
+
+        var response = chatGpt.completion(getBearer(),
+            ChatGptRequest.newRequest(openaiModel, promt))
+            .choices();
         
-        var promt = "Erstelle eine Lektion für die Themengruppe " + topic;
+        return response.get(0).message().content();
+
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/create-exam")
+    public String createExam(String topic) {
+
+        //tip, wie ein prompt aussehen könnte:
+        //`Imagine yourself as a teacher in a German Middle school from grade ${grade}. Your thoughts right now are that you want to create an exercise over: ${topic} in the Subject ${subject} based on the Online Reference: ${reference}. The tasksheet should be A4 size big with text size 12 and should include different tasks to train the subject as a whole. Make more tasks. Just show the sources, A4 tasks and solution sheet. Make more tasks. Return the answer as a template string of the html styled version. for example <h1>Hello</h1> would be a heading in the response string. Answer in German and leave out unnessecary formal contents.`;
+        var promt = String.format("""
+            Imagine yourself as a teacher in a German Middle school from grade 5. 
+        Your thoughts right now are that you want to create an exercise over: %s 
+        The tasksheet should be A4 size big with text size 12 and should include different tasks to train the subject as a whole. 
+        Make more tasks. Just show the sources, A4 tasks and solution sheet. 
+        Make more tasks. Return the answer as a template string of the html styled version. 
+        for example <h1>Hello</h1> would be a heading in the response string. 
+        Answer in German and leave out unnessecary formal contents.""", topic);
+                
 
         var response = chatGpt.completion(getBearer(),
             ChatGptRequest.newRequest(openaiModel, promt))
